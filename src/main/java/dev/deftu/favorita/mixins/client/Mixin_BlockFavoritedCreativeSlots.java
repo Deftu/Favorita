8e0943fc7f5cadbe04c8fa2e6557df2c3c7b5312
Deftu <deftudev@gmail.com>
2024-06-29 20:51:14 +0200

    Block the trash button from wiping favourited slots

--- src/main/java/dev/deftu/favorita/mixins/client/Mixin_BlockFavoritedCreativeSlots.java
+++ src/main/java/dev/deftu/favorita/mixins/client/Mixin_BlockFavoritedCreativeSlots.java
@@ -11,10 +11,13 @@ import dev.deftu.favorita.client.utils.MiscUtils;
 import dev.deftu.favorita.client.utils.SoundUtils;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreativeInventoryScreen.class)
@@ -59,6 +62,32 @@ public class Mixin_BlockFavoritedCreativeSlots {
             SoundUtils.playBlockedSound();
             MiscUtils.notifyBlocked();
         }
+    }
+
+    @Redirect(
+            //#if MC >= 1.16.5
+            method = "onMouseClick",
+            //#else
+            //$$ method = "handleMouseClick",
+            //#endif
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;clickCreativeStack(Lnet/minecraft/item/ItemStack;I)V",
+                    ordinal = 0
+            )
+    )
+    private void favorita$onItemWiped(ClientPlayerInteractionManager instance, ItemStack itemStack, int index) {
+        //#if MC >= 1.16.5
+        HandledScreen<?> $this = (HandledScreen<?>) (Object) this;
+        //#else
+        //$$ GuiContainer $this = (GuiContainer) (Object) this;
+        //#endif
+        int slotIndex = InventoryUtils.getSavedSlotIndex($this, index);
+        if (FavoritaConfig.isFavorited(slotIndex)) {
+            return;
+        }
+
+        instance.clickCreativeStack(itemStack, index);
     }
 
 }
